ab2c64689e26d2200932952e609dd1f003efa915
raunak234362 <raunakdob@gmail.com>
2023-08-21 15:27:39 +0530

    final comment

--- index.js
+++ index.js
@@ -26,55 +26,6 @@ app.set('view engine','ejs');
 app.set('views','./views');
 
 
-// app.get('/',function(req,res){
-//     Task.find({},function(err,task){
-//         if(err){
-//             console.log('Error in fetching tasks from db');
-//             return;
-//         }
-//         return res.render('home',{
-//             title:'Home',
-//             task:task
-//         });
-//     });
-// });
-
-// //create a new task
-// app.post('/create-task',function(req,res){
-
-//     Task.create({
-//         description:req.res.body.description,
-//         category:req.body.category,
-//         date:req.body.date
-//     },function(err,newtask){
-//         if(err){
-//             console.log('Error in creating task',err);
-//             return;
-//         }
-//         return res.redirect('back');
-//     });
-// });
-
-// // deleting Tasks
-// app.get('/delete-task', function (req, res) {
-//     // get the id from query
-//     var id = req.query;
-
-//     // checking the number of tasks selected to delete
-//     var count = Object.keys(id).length;
-//     for (let i = 0; i < count; i++) {
-
-//         // finding and deleting tasks from the DB one by one using id
-//         Task.findByIdAndDelete(Object.keys(id)[i], function (err) {
-//             if (err) {
-//                 console.log('error in deleting task');
-//             }
-//         })
-//     }
-//     return res.redirect('back');
-// });
-
-
 // Fetch tasks using promises
 app.get('/', function (req, res) {
     Task.find()
